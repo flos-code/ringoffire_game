40ba4143fb5ff3c7f6c07cb18b63f597ef9c3d16
flos-code <148456982+flos-code@users.noreply.github.com>
2023-12-28 14:29:00 +0100

    added player

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,18 +1,44 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   pickCardAnimation = false;
-  constructor() {}
-  ngOnInit(): void {}
+  game: Game;
+  currentCard: string = '';
+
+  constructor() {
+    this.game = new Game();
+  }
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      let poppedCard = this.game.stack.pop();
+      if (typeof poppedCard === 'string') {
+        this.currentCard = poppedCard;
+      }
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
   }
 }
---
+++ src/app/player/player.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './player.component.html',
+  styleUrl: './player.component.scss',
+})
+export class PlayerComponent implements OnInit {
+  @Input() name: string;
+  constructor() {
+    this.name = '';
+  }
+
+  ngOnInit(): void {}
+}
